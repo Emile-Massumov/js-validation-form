ca23ee4569f4c20f64b070a09926a50c52d3caa0
Émile <85419994+Emile-Massumov@users.noreply.github.com>
2022-02-22 19:37:31 -0500

    added main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+
+let username = id("username"),
+ email = id("email"),
+ password = id("password"),
+ form = id("form"),
+ errorMsg = classes("error"),
+ successIcon = classes("success-icon"),
+ failureIcon = classes("failure-icon");
+
+ form.addEventListener("submit", (e) => {
+     e.preventDefault();
+     
+     if(username.value === "") {
+         errorMsg[0].innerHTML = "User Name cannot be blank";
+         failureIcon[0].style.opacity = "1";
+         successIcon[0].style.opacity = "0";
+          }else{ 
+              errorMsg[0].innerHTML = "";
+              failureIcon[0].style.opacity = "0";
+              successIcon[0].style.opacity = "1";
+     }
+ });
+
